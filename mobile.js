e2e7233e1df9abf3b07349720371af1cc7485d1b
Molly Mason <mmaso004@gold.ac.uk>
2019-08-30 20:21:22 +0100

    starting mobile optimisation

---
+++ mobile.js
@@ -0,0 +1,10 @@
+/* Script from w3schools */
+
+function myFunction() {
+  var x = document.getElementById("hamlinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
